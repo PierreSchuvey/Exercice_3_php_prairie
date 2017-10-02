25df8cdd264c2a4a5316617188acf0215e3d9f58
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-10-02 15:24:53 +0200

    optimisation de l'exercice 3 PHP de la prairie

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-$min = 0;
-$max = 300;
-$chiffresImpairs = '';
-for ($i = $min; $i <= $max; $i++) {
-    if ( $i % 2 == 1 ) {
-        $chiffresImpairs .= $i.' ';
+$oddNumber = '';
+for ($oddNumber = 0; $oddNumber <= 300; $oddNumber++) {
+    if ( $oddNumber % 2 == 1 ) {
+        echo $oddNumber . ' ';
     }
 }
-echo "Les chiffres pairs compris entre $min et $max sont: $chiffresImpairs";
 ?>
